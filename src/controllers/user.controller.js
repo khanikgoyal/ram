8bac728efcd77abd889af85dcf21d2931a6762ac
khanikgoyal <khanikgoyal87@gmail.com>
2024-02-09 23:36:09 +0530

    sixth commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -208,7 +208,185 @@ const refreshAccessToken = asyncHandler(async (req, res )=>{
     }
 })
 
-export { registerUser ,loginUser, logoutUser, refreshAccessToken}
+const changeCurrentPassword= asyncHandler(async(req, res)=>{
+    const {oldPassword, newPassword}= req.body
+    const user = await User.findById(req.user?._id)
+    const isPassworsCorrect=await user.isPasswordCorrect(oldPassword)
+    if(!isPassworsCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password=newPassword
+    await user.save({validateBeforSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
+})
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName, email}= req.body
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields are required")
+    }
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullName,
+                email,
+            }
+        },
+        {new : true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+})
+
+const updateUserAvatar= asyncHandler(async(req, res)=>{
+    const avatatLocalPath = req.file?.path
+
+    if(!avatatLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar=await uploadOnCloudinary(avatatLocalPath)
+
+
+    if(!avatar){
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }   
+        },
+        {new :true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated")
+    )
+})
+
+const updateUserCoverImage= asyncHandler(async(req, res)=>{
+    const coverImageLocalPath = req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover Image file is missing")
+    }
+
+    const coverImage=await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage){
+        throw new ApiError(400, "Error while uploading on cover image")
+    }
+
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }   
+        },
+        {new :true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "cover image updated")
+    )
+})
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+        const {username}=req.params
+        if(!username?.trim()){
+            throw new ApiError(400, "username is missing")
+        }
+
+        //aggragate
+
+        const channel = await User.aggregate([
+            {
+                $match :{
+                    username:username?.toLowerCase()
+                }
+            },
+            {
+                $loookup:{
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as:"subscribe"
+                }
+            },
+            {
+                $loookup:{
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as:"subscribedTo"
+                }
+            },
+            {
+                $addFields: {
+                    subscribersCount: {
+                        $size: "$subscribers"
+                    },
+                    channelsSubscribedToCount:{
+                        $size:"$subscribedTo"
+                    },
+                    isSubscribed:{
+                        $cond:{
+                            if:{$in:[req.user?._id, "$subscribers.subscriber"]},
+                            then:true,
+                            else:false
+
+                        }
+                    }
+                }
+            },
+            {
+                $project:{
+                    fullName:1,
+                    username:1,
+                    subscribersCount:1,
+                    channelsSubscribedToCount:1,
+                    isSubscribed:true,
+                    avatar:1,
+                    coverImage:1,
+                    email:1
+
+                }
+            }
+        ])
+})
+
+export { 
+    registerUser,
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser,
+    updateUserAvatar,
+    updateUserCoverImage,
+    updateAccountDetails
+}
 
 
 
